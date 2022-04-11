4a2bd3ee31a6431626f0c00d9fecdaddc5a2a63d
José Alberto García Ochoa <josealbertogarcia453@gmail.com>
2022-04-10 23:47:22 -0500

    Errors

---
+++ errors.js
@@ -0,0 +1,33 @@
+//Strict mode
+/*function puedesDetectarElProblema() {
+    "use strict";
+    for (let contador = 0; contador < 10; contador++) { // <- Contador is not defined
+        console.log("Feliz feliz"); 
+    }
+}
+    
+puedesDetectarElProblema();
+
+function pedirDireccion(pregunta) {
+    //let resultado = prompt(pregunta);  <- prompt is not defined
+    if (resultado.toLowerCase() == "izquierda") return "I"; 
+    if (resultado.toLowerCase() == "derecha") return "D"; 
+    throw new Error("Dirección invalida: " + resultado);
+}
+    
+function mirar() {
+    if (pedirDireccion("Hacia que dirección quieres ir?") == "I") {
+        return "una casa"; 
+    } else {
+        return "dos osos furiosos"; 
+    }
+}
+
+try {
+    console.log("Tu ves", mirar());
+} catch (error) {
+    console.log("Algo incorrecto sucedio: " + error);
+}*/
+
+    
+
