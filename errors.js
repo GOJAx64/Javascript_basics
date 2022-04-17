60a3656a3bedc564999d204e2c1f481aa6596ab7
José Alberto García Ochoa <josealbertogarcia453@gmail.com>
2022-04-16 23:42:01 -0500

    Retry Excersice

--- errors.js
+++ errors.js
@@ -27,7 +27,55 @@ try {
     console.log("Tu ves", mirar());
 } catch (error) {
     console.log("Algo incorrecto sucedio: " + error);
-}*/
+}
+
+function transferir(desde, cantidad) {
+    if (cuentas[desde] < cantidad) return;
+    let progreso = 0;
+    try {
+        cuentas[desde] -= cantidad;
+        progreso = 1;
+        cuentas[obtenerCuenta()] += cantidad;
+        progreso = 2;
+    } finally {
+        if (progreso == 1) {
+            cuentas[desde] += cantidad;
+        }
+    } 
+}
+*/
+
+//retry
+function multiplication(x, y) {
+    if (Math.random() < 0.21) {
+        return x * y;
+    } else {
+        throw new TypeError("MultiplierUnitFailure");
+    }
+}
+
+
+while(true) {
+    try {
+        console.log(multiplication(2,5));
+        break;
+    } catch(error) {
+        console.log(error.message);
+    }
+}
+
+/*const caja = {
+    bloqueada: true,
+    desbloquear() { this.bloqueada = false; },
+    bloquear() { this.bloqueada = true;  },
+    _contenido: [],
+    get contenido() {
+        if (this.bloqueada) throw new Error("Bloqueada!");
+        return this._contenido;
+    }
+ };
+
+ function */
 
     
 
