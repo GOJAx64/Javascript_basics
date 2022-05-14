090f4688243d19e14ecba69b7723495565b0d6bd
José Alberto García Ochoa <josealbertogarcia453@gmail.com>
2022-05-13 20:42:21 -0500

    CallBacks, Promises, Async and Await

---
+++ 6 asynchronous_programming/callbacks.js
@@ -0,0 +1,28 @@
+function cuadradoCallback(value, callback) {
+    setTimeout(() => {
+      callback(value, value * value);
+    }, 0 | (Math.random() * 1000));
+  }
+  
+  cuadradoCallback(0, (value, result) => {
+    console.log("Inicia Callback");
+    console.log(`Callback: ${value}, ${result}`);
+    cuadradoCallback(1, (value, result) => {
+      console.log(`Callback: ${value}, ${result}`);
+      cuadradoCallback(2, (value, result) => {
+        console.log(`Callback: ${value}, ${result}`);
+        cuadradoCallback(3, (value, result) => {
+          console.log(`Callback: ${value}, ${result}`);
+          cuadradoCallback(4, (value, result) => {
+            console.log(`Callback: ${value}, ${result}`);
+            cuadradoCallback(5, (value, result) => {
+              console.log(`Callback: ${value}, ${result}`);
+              console.log("Fin Callback");
+              console.log("Callback Hell !!!!!😈🤘");
+              console.log("http://callbackhell.com/");
+            });
+          });
+        });
+      });
+    });
+  });
