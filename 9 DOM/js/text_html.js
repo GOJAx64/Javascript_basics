5dc9eb01e652756ab35af19d3cc2710781da3d01
José Alberto García Ochoa <josealbertogarcia453@gmail.com>
2022-05-30 20:59:29 -0500

    Attributes, styles CSS, classes CSS, text in HTML

---
+++ 9 DOM/js/text_html.js
@@ -0,0 +1,17 @@
+const $whatIsDOM = document.getElementById("que-es");
+let text = `
+    <p>
+      El Modelo de Objetos del Documento (<b><i>DOM - Document Object Model</i></b>) es un API para documentos HTML y XML.
+    </p>
+    <p>
+      Éste proveé una representación estructural del documento, permitiendo modificar su contenido y presentación visual mediante código JS.
+    </p>
+    <p>
+      <mark>El DOM no es parte de la especificación de JavaScript, es una API para los navegadores.</mark>
+    </p>
+  `;
+
+//$whatIsDOM.innerText = text;
+$whatIsDOM.textContent = text;
+$whatIsDOM.innerHTML = text;
+$whatIsDOM.outerHTML = text;
